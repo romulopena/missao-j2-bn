e1bff6b8c8707ce8dc343c29df3bed7d9e74b96a
PROF ROMULO <rjlpena@gmail.com>
2024-06-25 19:40:53 -0300

    criando objetos

---
+++ script.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".textoResultado");
+
+const perguntas = [
+    {
+        enunciado: "Pergunta1",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+    {
+        enunciado: "Pergunta2",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+    {
+        enunciado: "Pergunta3",
+        alternativas: ["Alternativa1", "Alternativa2"]
+    },
+];
