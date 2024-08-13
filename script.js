8e434ca1da72b9e307327cf6adc93e982700cbf5
PROF ROMULO <rjlpena@gmail.com>
2024-08-13 19:19:47 -0300

    R8

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const perguntas = [
                 texto: "Com a evolução das profissões e a  inclusão digital ao desenvolver tecnologias acessíveis para comunidades remotas pode reduzir a dependência de combustíveis fósseis e mitigando os efeitos do aquecimento global.",
                 afirmacao: "Quanto maior o acesso a tecnologia da população, maior será a probabilidade de desenvolvimento  da sociedade."
             },
-            {         
+            {
                 texto: "O pensamento evoluir faz-se crescer a buscar para soluções que levarão a evolução tanto da população quanto resolução de problemas complexos",
                 afirmacao: "Com  a população crescendo e evoluindo se encontra soluções não vistas hoje sem existir as tecnologias e simples atualização e crescimento"
             },
@@ -23,13 +23,13 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Acesso à Educação e Treinamento, empreendedorismo e inovação, Trabalho remoto e Freelancer",
-                afirmacao:"Cursos online, tutoriais, impulsionam a inovação e a colaboração em prol de um futuro sustentável"
+                afirmacao: "Cursos online, tutoriais, impulsionam a inovação e a colaboração em prol de um futuro sustentável"
             },
             {
                 texto: "Acessibilidade com tecnologias assistivas, comunidades online, facilitando o acesso ao conhecimento.",
                 afirmacao: "Aplicativos de reciclagem ajudam as pessoas e encontrar pontos de coleta."
             },
-            ]
+        ]
     },
     {
         enunciado: "Nos dias atuais percebemos que diversas alterações climáticas estão causando desastres naturais com uma intensidade que era esperada para daqui a 20 anos. Cada vez mais estão se confirmando episódios de desastres naturais devido ao aquecimento global. Esse cenário demanda o surgimento de novas profissões que venham a mitigar esse quadro. Para o seu futuro, você escolherá qual tipo de profissão?",
@@ -39,7 +39,7 @@ const perguntas = [
                 afirmacao: "Com o avanço dos desastres naturais, condições básicas de existência se tornaram mais escassas (alimentos, água potável e saneamento)."
             },
             {
-                texto:"Redução do surgimento de novas epidemias que tendem a surgir como consequênica do aquecimento global. Novos empregos surgem, novas possibilidades de trabalhos.",
+                texto: "Redução do surgimento de novas epidemias que tendem a surgir como consequênica do aquecimento global. Novos empregos surgem, novas possibilidades de trabalhos.",
                 afirmacao: "Redução do surgimento de novas epidemias que tendem a surgir como consequênica do aquecimento global. Novos empregos surgem, novas possibilidades de trabalhos."
             }
         ]
@@ -48,22 +48,29 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
-function mostraPergunta(){
+let historiaFinal = "";
+
+function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
-function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", ()=> respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
-    }
+}
+mostraPergunta();
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
     mostraPergunta();
+
+}
